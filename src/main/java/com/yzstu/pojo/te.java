4c0348e7d517491f3331813f31cb36659722a5ae
yz <1341251795@qq.com>
2022-07-13 15:31:18 +0800

    three commit

--- src/main/java/com/yzstu/pojo/te.java
+++ src/main/java/com/yzstu/pojo/te.java
@@ -7,7 +7,7 @@ public class te {
     public static void main(String[] args) {
         System.out.println("hello  git");
         System.out.println("hello  git002");
-        //System.out.println("hello  git");
+        System.out.println("hello  git");
 
     }
 }
